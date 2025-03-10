7df4f31d435c921bbe653e49ba0ec118bfdb7fb9
Alex Drummond <a.d.drummond@gmail.com>
2025-03-10 12:44:50 +0000

    initial commit

---
+++ init.el
@@ -0,0 +1,81 @@
+(setq inhibit-startup-screen t)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(toggle-scroll-bar -1)
+(setq exec-path (append exec-path '("~/bin")))
+
+; allow '#' entry on Mac UK keyboard
+(global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
+
+; allow use of a single press of ESC to cancel stuff
+(define-key global-map (kbd "<escape>") 'keyboard-escape-quit)    
+
+(defun open-init-el()
+  (interactive)
+  (find-file user-init-file)
+  )
+
+; TODO not sure if this is necessary in emacs 30
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(require 'evil)
+(evil-mode 1)
+
+; allow undoing window configuration changes
+(winner-mode 1)
+(keymap-global-set "s-[" 'winner-undo)
+
+; override default key bindings to allow C-a and C-e to move to beginning and
+; end of line
+(use-package evil
+  :bind (:map evil-motion-state-map
+	      ("s-." . open-init-el)
+              ("C-a" . move-beginning-of-line)
+              ("C-e" . move-end-of-line)))
+
+(setopt display-fill-column-indicator-column 80)
+(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
+
+(load-theme 'afternoon t)
+
+(use-package go-ts-mode
+  :ensure t
+  :preface
+  (setq treesit-language-source-alist '())
+  (defun ad/go-presave()
+    (lsp-organize-imports)
+    (lsp-format-buffer)
+    )
+  (defun ad/go-lsp-start()
+    (setq tab-width 2)
+    (add-hook 'before-save-hook #'ad/go-presave t t)
+    (lsp-deferred)
+   )
+  (defun ad/golang-ts-init()
+    (add-to-list 'treesit-language-source-alist '(go "https://github.com/tree-sitter/tree-sitter-go"))
+    (add-to-list 'treesit-language-source-alist '(gomod "https://github.com/camdencheek/tree-sitter-go-mod"))
+    ;(dolist (lang '(go gomod)) (treesit-install-language-grammar lang))
+    (add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode))
+    (add-to-list 'auto-mode-alist '("/go\\.mod\\'" . go-mod-ts-mode))
+  )
+  :init (ad/golang-ts-init)
+  :hook
+  (go-ts-mode . ad/go-lsp-start)
+)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(afternoon-theme evil flycheck lsp-mode lsp-ui magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
