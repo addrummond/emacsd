4fad9030874ece137bec79014b2e89e8d11e3bd0
Alex Drummond <a.d.drummond@gmail.com>
2025-03-10 20:31:00 +0000

    add command for formatting elisp code

--- init.el
+++ init.el
@@ -5,22 +5,21 @@
 (toggle-scroll-bar -1)
 (setq exec-path (append exec-path '("~/bin")))
 
-; allow '#' entry on Mac UK keyboard
+;; allow '#' entry on Mac UK keyboard
 (global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
 
-; allow use of a single press of ESC to cancel stuff
+;; allow use of a single press of ESC to cancel stuff
 (define-key global-map (kbd "<escape>") 'keyboard-escape-quit)    
 
-; ensure absence of obnoxious bell
+;; ensure absence of obnoxious bell
 (setq visible-bell nil)
 (setq ring-bell-function #'ignore)
 
 (defun open-init-el()
   (interactive)
-  (find-file user-init-file)
-  )
+  (find-file user-init-file))
 
-; TODO not sure if this is necessary in emacs 30
+;; TODO not sure if this is necessary in emacs 30
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
@@ -28,17 +27,25 @@
 (require 'evil)
 (evil-mode 1)
 
-; allow undoing window configuration changes
+;; allow undoing window configuration changes
 (winner-mode 1)
 (keymap-global-set "s-[" 'winner-undo)
 
+;; so far just used for elisp
+(defun ad/manually-format-whole-file ()
+  (cond
+   ((eq major-mode 'emacs-lisp-mode)
+    (indent-region (point-min) (point-max)))))
+
 (use-package evil
-  :bind (:map evil-motion-state-map
-	      ("s-." . open-init-el)
-	      ("s-;" . fill-paragraph)
-              ("C-a" . move-beginning-of-line)
-              ("C-e" . move-end-of-line)
-	      ("U" . undo-redo)))
+  :bind (:map
+	 evil-motion-state-map
+	 ("s-." . open-init-el)
+	 ("s-;" . fill-paragraph)
+	 ("s-\\" . (lambda () (interactive) (ad/manually-format-whole-file)))
+         ("C-a" . move-beginning-of-line)
+	 ("C-e" . move-end-of-line)
+	 ("U" . undo-redo)))
 
 (setopt display-fill-column-indicator-column 80)
 (add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
@@ -52,24 +59,20 @@
   (setq treesit-language-source-alist '())
   (defun ad/go-presave()
     (lsp-organize-imports)
-    (lsp-format-buffer)
-    )
+    (lsp-format-buffer))
   (defun ad/go-lsp-start()
     (setq tab-width 2)
     (add-hook 'before-save-hook #'ad/go-presave t t)
-    (lsp-deferred)
-   )
+    (lsp-deferred))
   (defun ad/golang-ts-init()
     (add-to-list 'treesit-language-source-alist '(go "https://github.com/tree-sitter/tree-sitter-go"))
     (add-to-list 'treesit-language-source-alist '(gomod "https://github.com/camdencheek/tree-sitter-go-mod"))
-    ;(dolist (lang '(go gomod)) (treesit-install-language-grammar lang))
+    ;;(dolist (lang '(go gomod)) (treesit-install-language-grammar lang))
     (add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode))
-    (add-to-list 'auto-mode-alist '("/go\\.mod\\'" . go-mod-ts-mode))
-  )
+    (add-to-list 'auto-mode-alist '("/go\\.mod\\'" . go-mod-ts-mode)))
   :init (ad/golang-ts-init)
   :hook
-  (go-ts-mode . ad/go-lsp-start)
-)
+  (go-ts-mode . ad/go-lsp-start))
 
 (defun ad/format-project (p)
   (if (null p)
